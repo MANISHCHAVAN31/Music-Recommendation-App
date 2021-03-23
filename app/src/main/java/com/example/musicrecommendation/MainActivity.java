c5cb2109fb82b69fd3c992de28787691ba623d4e
Manish <manishchavan80@gmail.com>
2021-03-23 19:48:02 +0530

    Image Quality Updated

--- app/src/main/java/com/example/musicrecommendation/MainActivity.java
+++ app/src/main/java/com/example/musicrecommendation/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.musicrecommendation;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,19 +23,31 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap bitmap;
     private ImageView imageView;
 
+    private String currentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
+
     }
 
     public void OpenCamera(View view) {
-        Intent imageCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String filename = "photo";
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
-        if(imageCaptureIntent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(imageCaptureIntent, REQUEST_IMAGE_CAPTURE);
+        try {
+            File imageFile = File.createTempFile(filename, ".jpeg", storageDirectory);
+
+            currentPhotoPath = imageFile.getAbsolutePath();
+            Uri imageUri = FileProvider.getUriForFile(MainActivity.this, "com.example.musicrecommendation.fileprovider", imageFile);
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(intent, 1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -36,13 +55,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
 
-            Intent intent = new Intent(MainActivity.this, ImageDisplay.class);
-            intent.putExtra("capturedImage", imageBitmap);
-            startActivity(intent);
+            imageView = findViewById(R.id.imageView);
+            imageView.setImageBitmap(bitmap);
         }
     }
 }
